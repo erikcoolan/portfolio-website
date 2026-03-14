48b51f2d243dfa1d936fcb492b15f268599ef597
erikcoolan <166562187+erikcoolan@users.noreply.github.com>
2026-03-13 20:08:50 -0400

    Add files via upload

---
+++ Website Script.js
@@ -0,0 +1,8 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    document.getElementById("formMessage").innerText =
+    "Thank you! Your message has been sent.";
+
+});
